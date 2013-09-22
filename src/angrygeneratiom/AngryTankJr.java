e1acea0b7d6c359078f121c32d1fb0014ea2ff09
Sylfer <sylfers@gmail.com>
2013-09-22 19:33:20 +0400

    Tank is repainted as red

--- src/angrygeneratiom/AngryTankJr.java
+++ src/angrygeneratiom/AngryTankJr.java
@@ -1,7 +1,11 @@
 package angrygeneratiom;
 
 import robocode.Robot;
+import java.awt.Color;
 
 public class AngryTankJr extends Robot {
 
+	 public void run() {
+		 setBodyColor(new Color(255, 0, 0));
+     }
 }
